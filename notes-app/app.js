c2c11beb5c06874e0447d28a65bfe3491d3cbf2e
JingWang <jingwang9302@gmail.com>
2020-02-01 17:11:22 -0800

    first commit

---
+++ notes-app/app.js
@@ -0,0 +1,98 @@
+const chalk = require('chalk')
+const notes = require('./nodes.js')
+const yargs = require('yargs')
+
+//customize yards version
+
+yargs.version('1.1.0')
+// add, remove, read, list
+
+// add
+yargs.command({
+    command:'add',
+    describe:'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type:'string',
+        },
+        body:{
+            describe:'Note body',
+            demandOption: true,
+            type:'string',
+
+        }
+    },
+    handler: function(argv){
+        notes.addNote(argv.title,argv.body)
+    }
+})
+
+//remove
+yargs.command({
+    command:'remove',
+    describe: 'Remove a new note',
+    builder:{
+        title:{
+            describe:'Note title',
+            demandOption:true,
+            type:'string',
+        }
+    },
+    handler: function(argv){
+        notes.removeNote(argv.title)
+    },
+})
+
+// list
+yargs.command({
+    command:'list',
+    describe:'List your notes',
+    handler: function(){
+        console.log("List the notes")
+    }
+})
+
+//read
+yargs.command({
+    command:'read',
+    describe:'Read your notes',
+    handler: function(){
+        console.log("Reading the notes")
+    }
+})
+
+
+// console.log(yargs.argv)
+yargs.parse()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const command = process.argv[2]
+// console.log(process.argv)
+
+// if (command === 'add'){
+//     console.log("adding note!")
+// } else if(command === 'remove'){
+//     console.log("remove note")
+// } else{
+//     console.log("good")
+// }
+
+
+// const add = require('./util.js')
+
+// const sum = add(2,3);
+// console.log(sum);
